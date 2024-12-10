91c934fbab976e6b195ae9d84997b77412ccf6fd
AssortedFood <hamoon.b.94@gmail.com>
2024-12-10 00:08:54 +0000

    add posts route to app.js

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ import mongoose from 'mongoose';
 const app = express();
 app.use(express.json());
 
+
 import authRoute from './routes/auth.js';
+import postsRoute from './routes/posts.js';
+
 app.use('/api/user',authRoute)
+app.use('/api/posts',postsRoute)
 
 mongoose.connect(process.env.DB_CONNECTOR).then(()=>{
     console.log('Your mongoDB connector is on...')
